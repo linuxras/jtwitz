3933d9b099f84f0a323aa557446d34f3a5aa4905
Andrew Williams <linuxras@gmail.com>
2010-07-06 18:35:20 -0500

    Fixed adding of the proper ViewportView on search type selection.

--- src/twitz/ui/SearchPanel.java
+++ src/twitz/ui/SearchPanel.java
@@ -421,6 +421,7 @@ public class SearchPanel extends javax.swing.JPanel implements TwitzEventModel {
 			{
 				tlm.addTweet(t);
 			}
+			searchPane.setViewportView(tweetList);
 		}
 	}
 
@@ -437,6 +438,7 @@ public class SearchPanel extends javax.swing.JPanel implements TwitzEventModel {
 			{
 				contactsList.addUser(u);
 			}
+			searchPane.setViewportView(contactsList);
 		}
 	}
 
