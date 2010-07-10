6053a9f9ceb79c54f20458f6ad04548f1af5edc9
Andrew Williams <linuxras@gmail.com>
2010-07-09 20:00:00 -0500

    Added dos batch file to launch Twitz

---
+++ twitz.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_20
+REM set JAVA_HOME=F:\Documents\jre
+set ANT_HOME=F:\Documents\apache-ant-1.8.1
+set PATH=%PATH%;%JAVA_HOME%\bin;%ANT_HOME%\bin
+
+IF "%1"=="" GOTO default
+IF "%1"=="run"
+java -jar dist\Twitz.jar
+IF "%1"=="build"
+ant jar
+REM cmd.exe
+REM java -cp "F:\Documents\Twitz\thirdparty\AppFramework.jar;F:\Documents\Twitz\thirdparty\twitter4j-core-2.1.2.jar;F:\Documents\Twitz\thirdparty\swingx-0.9.4.jar;F:\Documents\Twitz\thirdparty\substance-tools.jar;F:\Documents\Twitz\thirdparty\substance-tst.jar;F:\Documents\Twitz\thirdparty\substance.jar;F:\Documents\Twitz\thirdparty\trident.jar;F:\Documents\Twitz\thirdparty\commons-logging-1.1.1.jar;F:\Documents\Twitz\${libs.swing-layout.classpath};F:\Documents\Twitz\${libs.absolutelayout.classpath};F:\Documents\Twitz\thirdparty\log4j-1.2.14.jar;F:\Documents\Twitz\dist\Twitz.jar" twitz.TwitzApp
+
+:default
+java -jar dist\Twitz.jar
